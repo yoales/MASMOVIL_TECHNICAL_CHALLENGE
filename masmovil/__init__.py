225317428e10f724a426ef4ef4880b9715fea35b
asancmun <alejandro.sanchezmunoz@orange.com>
2020-12-16 21:12:16 +0100

    Segunda Parte: Python. 4

--- masmovil/__init__.py
+++ masmovil/__init__.py
@@ -7,7 +7,7 @@ class Persona:
         print(f"Hola! Soy {self.nombre} y tengo {self.edad} años")
 
 class Trabajador(Persona):
-    def __init__(self, nombre, edad, departamento, puesto):
+    def __init__(self, nombre, edad, departamento = 'Data', puesto = 'Analyst'):
         Persona.__init__(self, nombre, edad)
 
         self.departamento = departamento
@@ -23,5 +23,5 @@ persona_1 = Persona(nombre, 20)
 persona_1.presentation()
 
 nombre = 'Alejandro'
-trabajador_1 = Trabajador(nombre, 30, 'Data', 'Analyst')
+trabajador_1 = Trabajador(nombre, 30)
 trabajador_1.presentation()
